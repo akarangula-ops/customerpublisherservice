cbc8c9a4f9cf435f92e9d1fba59130bc03da8c0b
akarangula-ops <akarangula@pkglobal.com>
2021-03-23 15:22:59 +0530

    Integration with Kafka

--- src/main/java/com/pkglobal/producer/customerpublisherservice/service/CustomerPublisherService.java
+++ src/main/java/com/pkglobal/producer/customerpublisherservice/service/CustomerPublisherService.java
@@ -3,14 +3,19 @@ package com.pkglobal.producer.customerpublisherservice.service;
 import com.pkglobal.producer.customerpublisherservice.domain.Customer;
 import com.pkglobal.producer.customerpublisherservice.domain.CustomerSuccessResponse;
 import com.pkglobal.producer.customerpublisherservice.exception.TokenException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerPublisherService {
 
+  @Autowired
+  private KafkaSender kafkaSender;
+
   public CustomerSuccessResponse publishToKafka(Customer body, String authorization) {
     CustomerSuccessResponse customerSuccessResponse = new CustomerSuccessResponse();
     if (authorization.length() > 10) {
+      kafkaSender.send(body.toString());
       customerSuccessResponse.setStatus("201");
       customerSuccessResponse.setMessage("published the message successfully to Kafka");
     } else {
